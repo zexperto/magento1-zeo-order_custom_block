3520a0e52cd01431aedbe5d94f37920e6283ecdb
zexperto <zexperto@hotmail.com>
2016-02-12 20:19:47 +0200

    Import Files from Existing Extension

---
+++ app/code/local/Zeo/OrderCustomBlock/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class Zeo_OrderCustomBlock_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 
---
+++ app/code/local/Zeo/OrderCustomBlock/Model/Adminhtml/Observer.php
@@ -0,0 +1,15 @@
+﻿<?php
+class Zeo_OrderCustomBlock_Model_Adminhtml_Observer{
+	public function addOrderCustomBlock(Varien_Event_Observer $observer){
+		$block = $observer->getBlock();
+		if(($block->getNameInLayout() == 'order_info') && ($child = $block->getChild('zeo.order.info.customblock'))){
+			$transport = $observer->getTransport();
+			if($transport){
+				$html = $transport->getHtml();
+				$html .= $child->toHtml();
+				$transport->setHtml($html);
+			}
+		}
+		
+	}
+}
